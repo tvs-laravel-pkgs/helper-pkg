4ab855f64fe77a05a51ce23cad6e4cebd1b17f26
Abdul <abdul@uitoux.in>
2020-01-13 11:51:25 +0530

    Update HelperPkgServiceProvider.php

--- src/HelperPkgServiceProvider.php
+++ src/HelperPkgServiceProvider.php
@@ -14,7 +14,7 @@ class HelperPkgServiceProvider extends ServiceProvider {
 		$this->loadRoutesFrom(__DIR__ . '/routes/web.php');
 		$this->loadRoutesFrom(__DIR__ . '/routes/api.php');
 		$this->loadMigrationsFrom(__DIR__ . '/database/migrations');
-		$this->loadViewsFrom(__DIR__ . '/views', 'helper');
+		$this->loadViewsFrom(__DIR__ . '/views', 'helper-pkg');
 		$this->publishes([
 			__DIR__ . '/public' => base_path('public'),
 		]);
