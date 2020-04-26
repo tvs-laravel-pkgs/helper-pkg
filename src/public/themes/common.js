dedadb46accedbda62b8294c43afe8f3e62dd440
Abdul <abdul@uitouxsolutions.com>
2020-04-26 18:28:29 +0530

    u

---
+++ src/public/themes/common.js
@@ -0,0 +1,58 @@
+function showCheckAllTabErrorNoty() {
+    custom_noty('error', 'You have errors, Please check all tabs')
+}
+
+function showServerErrorNoty() {
+    custom_noty('error', 'Something went wrong at server')
+}
+
+
+function showErrorNoty(res) {
+    custom_noty('error', res.error);
+
+    var errors = '';
+    for (var i in res.errors) {
+        errors += '<li>' + res.errors[i] + '</li>';
+    }
+    if (errors) {
+        custom_noty('error', errors);
+    }
+}
+
+function url(url) {
+    if (base_url.charAt(base_url.length) == '/') {
+        return base_url + url;
+    } else {
+        return base_url + '/' + url;
+    }
+}
+
+app.directive('presetFilterSelect', function() {
+    return {
+        templateUrl: preset_filter_select_template_url,
+        controller: function() {
+            var self = this;
+            self.theme = theme;
+        }
+    }
+});
+
+app.directive('filterBtn', function() {
+    return {
+        templateUrl: filter_btn_template_url,
+        controller: function() {
+            var self = this;
+            self.theme = theme;
+        }
+    }
+});
+
+app.directive('deleteConfirmModal', function() {
+    return {
+        templateUrl: delete_confirm_modal_template_url,
+        controller: function() {
+            var self = this;
+            self.theme = theme;
+        }
+    }
+});
--- src/views/setup.blade.php
+++ src/views/setup.blade.php
@@ -19,42 +19,22 @@
     @if(Auth::user())
         var user_id = {{Auth::id()}};
     @endif
+
+    var preset_filter_select_template_url = "{{asset($helper_pkg_prefix.'/public/themes/'.$theme.'/preset-filter-select.html')}}";
+    var filter_btn_template_url = "{{asset($helper_pkg_prefix.'/public/themes/'.$theme.'/filter-btn.html')}}";
+    var delete_confirm_modal_template_url = "{{asset($helper_pkg_prefix.'/public/themes/'.$theme.'/delete-confirm-modal.html')}}";
+
+
+
+
 </script>
 <script src="{{ asset($helper_pkg_prefix.'/public/angular/helper-pkg/angular-setup.js')}}"></script>
+<script src="{{asset($helper_pkg_prefix.'/public/themes/common.js')}}"></script>
 
 <script data-require="underscore.js@*" data-semver="1.5.1" src="https://underscorejs.org/underscore-min.js"></script>
 <script type="text/javascript" src="{{URL::asset($helper_pkg_prefix.'/public/angular/helper-pkg/ng-shortcut.js?v=2')}}"></script>
 
 <script type="text/javascript">
-    function showCheckAllTabErrorNoty(){
-        custom_noty('error', 'You have errors, Please check all tabs')
-    }
-
-    function showServerErrorNoty(){
-        custom_noty('error', 'Something went wrong at server')
-    }
-
-
-    function showErrorNoty(res){
-        custom_noty('error', res.error);
-
-        var errors = '';
-        for (var i in res.errors) {
-            errors += '<li>' + res.errors[i] + '</li>';
-        }
-        if (errors) {
-            custom_noty('error', errors);
-        }
-    }
-
-    function url(url){
-        if(base_url.charAt(base_url.length) == '/'){
-            return base_url+url;
-        }else{
-            return base_url+'/'+url;
-        }
-    }
-
     var laravel_routes = [];
     @foreach(Route::getRoutes()->getRoutesByName() as $route_name => $route)
         laravel_routes['{{$route_name}}'] = url('{{$route->uri}}');
